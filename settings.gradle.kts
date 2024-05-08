8f21ebff25f73fcb7d21b49f524e2a7f4739a587
Bram <bram@bramdekker.com>
2024-05-08 13:29:51 +0200

    Added: Gradle Enterprise for build scanning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
+plugins {
+    id("com.gradle.enterprise") version("3.16.2")
+}
+
 rootProject.name = "PlayerCrasher"
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
