5664d9582ddc0d8857efd93030f5ccc96d22390d
Bram <bram@bramdekker.com>
2024-05-09 00:28:54 +0200

    Updated: PacketEvents -> 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.20.6-R0.1-SNAPSHOT")
-    compileOnly("com.github.retrooper.packetevents:spigot:2.2.1")
+    compileOnly("com.github.retrooper.packetevents:spigot:2.3.0")
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
 }
